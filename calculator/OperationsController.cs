dbe3c0d677ccbeb5247729645de81ea62e9e997f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2025-08-12 08:40:17 +0200

    create method to check if operator already exists

--- calculator/OperationsController.cs
+++ calculator/OperationsController.cs
@@ -8,6 +8,16 @@ namespace calculator
 {
     internal class OperationsController
     {
+        public bool OperatorDoesNotExistInDisplay(string displayText)
+        {
+            if ((!displayText.Contains('+') && !displayText.Contains('-') && !displayText.Contains('÷') && !displayText.Contains('×')) && displayText.Any(char.IsDigit))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
         public string Addition(string displayText)
         {
             string[] operands = displayText.Split('+');
@@ -15,6 +25,15 @@ namespace calculator
             decimal operand2 = decimal.TryParse(operands[1], out decimal parsedValue) ? parsedValue : 0;
 
             return $"{operand1 + operand2}";
+        }
+
+        public string Subtraction(string displayText)
+        {
+            string[] operands = displayText.Split('-');
+            decimal operand1 = decimal.Parse(operands[0]);
+            decimal operand2 = decimal.TryParse(operands[1], out decimal parsedValue) ? parsedValue : 0;
+
+            return $"{operand1 - operand2}";
         }
     }
 }
